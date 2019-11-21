7034a1ef70544c28f60033d3a7bcfd049bd0c7d2
Johnatan Lima <joh23nathan@gmail.com>
2019-11-21 20:32:01 -0300

    Configurações de mapping

--- Models/Maps/RealizadorMap.cs
+++ Models/Maps/RealizadorMap.cs
@@ -16,10 +16,13 @@ namespace Blevent.Models.Maps
             builder.Property(p => p.Telefone).HasColumnType("varchar(14)").HasMaxLength(14);
 
             builder.Property(p => p.Email).HasColumnType("varchar(20)").HasMaxLength(20).IsRequired();
-
+            
             //Props de Navegação e Relacionamentos
             //Geradas automaticamente pelo EF Core
-
+            builder.HasMany(x => x.Eventos)
+                .WithOne(x => x.RealizadorVirtual)
+                .HasForeignKey(x => x.RealizadorId)
+                .OnDelete(DeleteBehavior.SetNull);
         }
     }
 }
